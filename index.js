a1763b89ec80f2bc38a16d219c44e262312820d4
Felipe-HVSilva <felype.hen@hotmail.com>
2024-06-18 16:49:03 -0300

    feat: add GetUserByIdController and validations

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import 'dotenv/config.js'
 import express from 'express'
 import { CreateUserController } from './src/controllers/create-user.js'
+import { GetUserByIdController } from './src/controllers/get-user-by-id.js'
 
 const app = express()
 
 app.use(express.json())
+
+app.get('/api/users/:userId', async (request, response) => {
+    const getUserByIdController = new GetUserByIdController()
+    const { statusCode, body } = await getUserByIdController.execute(request)
+
+    response.status(statusCode).json(body)
+})
 
 app.post('/api/users', async (request, response) => {
     const createUserController = new CreateUserController()
---
+++ src/controllers/get-user-by-id.js
@@ -0,0 +1,26 @@
+import validator from 'validator'
+import { GetUserByIdUseCase } from '../use-cases/get-user-by-id.js'
+import { badRequest, ok, serverError } from './helpers.js'
+
+export class GetUserByIdController {
+    async execute(httpRequest) {
+        try {
+            const isIdValid = validator.isUUID(httpRequest.params.userId)
+
+            if (!isIdValid) {
+                return badRequest({ message: 'The provided is not a valid' })
+            }
+
+            const userId = httpRequest.params.userId
+
+            const getUserByIdUseCase = new GetUserByIdUseCase()
+
+            const user = await getUserByIdUseCase.execute(userId)
+
+            return ok(user)
+        } catch (error) {
+            console.error(error)
+            return serverError()
+        }
+    }
+}
