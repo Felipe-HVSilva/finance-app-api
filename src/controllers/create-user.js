ae110732152291b72c55f2528cb578bfabbf836d
Felipe-HVSilva <felype.hen@hotmail.com>
2024-06-19 02:13:10 -0300

    feat: add helper function to user controller

--- src/controllers/create-user.js
+++ src/controllers/create-user.js
@@ -1,7 +1,12 @@
-import validator from 'validator'
 import { CreateUserUseCase } from '../use-cases/create-user.js'
-import { badRequest, created, serverError } from './helpers.js'
+import { badRequest, created, serverError } from './helpers/http.js'
 import { EmailAlreadyInUseError } from '../errors/user.js'
+import {
+    checkIsEmailIsValid,
+    checkIsPasswordIsValid,
+    emailIsAlreadyInUseResponse,
+    invalidPasswordResponse,
+} from './helpers/user.js'
 
 export class CreateUserController {
     async execute(httpRequest) {
@@ -21,20 +26,16 @@ export class CreateUserController {
                 }
             }
 
-            const passwordIsNotValid = params.password.length < 6
+            const passwordIsValid = checkIsPasswordIsValid(params.password)
 
-            if (passwordIsNotValid) {
-                return badRequest({
-                    message: 'Password must be at least 6 characters',
-                })
+            if (!passwordIsValid) {
+                invalidPasswordResponse()
             }
 
-            const emailIsValid = validator.isEmail(params.email)
+            const emailIsValid = checkIsEmailIsValid(params.email)
 
             if (!emailIsValid) {
-                return badRequest({
-                    message: 'Invalid e-mail. Please provide a valid one.',
-                })
+                emailIsAlreadyInUseResponse()
             }
 
             const createUserUseCase = new CreateUserUseCase()
---
+++ src/controllers/helpers/user.js
@@ -0,0 +1,16 @@
+import validator from 'validator'
+import { badRequest } from './http.js'
+
+export const invalidPasswordResponse = () => {
+    return badRequest({ message: 'Password must be at least 6 characters.' })
+}
+
+export const emailIsAlreadyInUseResponse = () =>
+    badRequest({ message: 'Invalid e-mail. Please provide a valid one.' })
+
+export const invalidIdResponse = () =>
+    badRequest({ message: 'The provided id is invalid.' })
+
+export const checkIsPasswordIsValid = (password) => password.length >= 6
+
+export const checkIsEmailIsValid = (email) => validator.isEmail(email)
