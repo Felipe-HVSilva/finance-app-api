bca8db62230938624d4c2e298c502fff28dba455
Felipe-HVSilva <felype.hen@hotmail.com>
2024-06-24 13:09:02 -0300

    refactor: crete generic validation helper function file

--- src/controllers/helpers/user.js
+++ src/controllers/helpers/user.js
@@ -1,10 +1,6 @@
 import validator from 'validator'
 import { badRequest } from './http.js'
 
-export const invalidPasswordResponse = () => {
-    return badRequest({ message: 'Password must be at least 6 characters.' })
-}
-
 export const emailIsAlreadyInUseResponse = () =>
     badRequest({ message: 'Invalid e-mail. Please provide a valid one.' })
 
@@ -14,5 +10,3 @@ export const invalidIdResponse = () =>
 export const checkIsPasswordIsValid = (password) => password.length >= 6
 
 export const checkIsEmailIsValid = (email) => validator.isEmail(email)
-
-export const checkIfIdIsValid = (id) => validator.isUUID(id)
