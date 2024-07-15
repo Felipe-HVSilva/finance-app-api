c7a3a180fa1f2dd769ac49440ba2f7d9a45ee377
Felipe-HVSilva <felype.hen@hotmail.com>
2024-07-15 15:39:32 -0300

    refactor: remove unused code

--- src/controllers/helpers/user.js
+++
@@ -1,9 +0,0 @@
-import { badRequest } from './http.js'
-
-export const emailIsAlreadyInUseResponse = () =>
-    badRequest({ message: 'Invalid e-mail. Please provide a valid one.' })
-
-export const invalidIdResponse = () =>
-    badRequest({ message: 'The provided id is invalid.' })
-
-export const checkIsPasswordIsValid = (password) => password.length >= 6
--- src/controllers/helpers/validation.js
+++ src/controllers/helpers/validation.js
@@ -12,26 +12,3 @@ export const requiredFieldsIsMissingResponse = (field) => {
         message: `The field ${field} is required`,
     })
 }
-
-export const checkIsString = (value) => typeof value === 'string'
-
-export const validateRequiredFiled = (params, requiredFields) => {
-    for (const field of requiredFields) {
-        const fieldIsMissing = !params[field]
-        const fieldIsEmpty =
-            checkIsString(params[field]) &&
-            validator.isEmpty(params[field], {
-                ignore_whitespace: true,
-            })
-        if (fieldIsMissing || fieldIsEmpty) {
-            return {
-                missingField: field,
-                ok: false,
-            }
-        }
-    }
-    return {
-        ok: true,
-        missingField: undefined,
-    }
-}
