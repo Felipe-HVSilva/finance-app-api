761947d3617aa759621bce9623efd37d39fcfebc
Felipe-HVSilva <felype.hen@hotmail.com>
2024-07-19 19:17:58 -0300

    test: ensure CreateTransactionUseCase throw if IdGeneratorAdapter throws

--- src/use-cases/transaction/create-transaction.test.js
+++ src/use-cases/transaction/create-transaction.test.js
@@ -127,4 +127,15 @@ describe('CreateTransactionUseCase', () => {
 
         await expect(promise).rejects.toThrow()
     })
+
+    it('should throw if IdGeneratorAdapter throws', async () => {
+        const { sut, idGeneratorAdapter } = makeSut()
+        jest.spyOn(idGeneratorAdapter, 'execute').mockImplementationOnce(() => {
+            throw new Error()
+        })
+
+        const promise = sut.execute(idGeneratorAdapter)
+
+        await expect(promise).rejects.toThrow()
+    })
 })
