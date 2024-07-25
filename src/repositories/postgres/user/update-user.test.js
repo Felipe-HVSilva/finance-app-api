d03b01bae1c291890f368fb92512c9a3119ef775
Felipe-HVSilva <felype.hen@hotmail.com>
2024-07-25 09:34:12 -0300

    test: ensure PostgresUpdateUserRepository calls prisma with correct param

--- src/repositories/postgres/user/update-user.test.js
+++ src/repositories/postgres/user/update-user.test.js
@@ -4,21 +4,36 @@ import { user as fakeUser } from '../../../tests/index.js'
 import { PostgresUpdateUserRepository } from './update-user.js'
 
 describe('PostgresUpdateUserRepository', () => {
+    const updateUser = {
+        id: faker.string.uuid(),
+        first_name: faker.person.firstName(),
+        email: faker.internet.email(),
+        last_name: faker.person.lastName(),
+        password: faker.string.uuid(),
+    }
+
     it('should update user on db', async () => {
         const user = await prisma.user.create({ data: fakeUser })
 
         const sut = new PostgresUpdateUserRepository()
 
-        const updateUser = {
-            id: faker.string.uuid(),
-            first_name: faker.person.firstName(),
-            email: faker.internet.email(),
-            last_name: faker.person.lastName(),
-            password: faker.string.uuid(),
-        }
-
         const result = await sut.execute(user.id, updateUser)
 
         expect(result).toStrictEqual(updateUser)
+    })
+
+    it('should call Prisma with correct params', async () => {
+        const user = await prisma.user.create({ data: fakeUser })
+        const sut = new PostgresUpdateUserRepository()
+        const prismaSpy = jest.spyOn(prisma.user, 'update')
+
+        await sut.execute(user.id, updateUser)
+
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                id: user.id,
+            },
+            data: updateUser,
+        })
     })
 })
