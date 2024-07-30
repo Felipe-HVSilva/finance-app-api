0da5afdaff77f31176d5a2c9f56164d5a1788b76
Felipe-HVSilva <felype.hen@hotmail.com>
2024-07-30 17:59:08 -0300

    test: ensure PATCH /api/users/:userId return 404 when user is not found

--- src/routes/user.e2e.test.js
+++ src/routes/user.e2e.test.js
@@ -131,4 +131,19 @@ describe('User Routes E2E Test', () => {
 
         expect(response.status).toBe(404)
     })
+
+    it('PATCH /api/users/:userId should return 404 when user is not found', async () => {
+        const updatedUserParams = {
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+        }
+
+        const response = await request(app)
+            .get(`/api/users/${faker.string.uuid()}`)
+            .send(updatedUserParams)
+
+        expect(response.status).toBe(404)
+    })
 })
