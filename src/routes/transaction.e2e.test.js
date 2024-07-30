9ea31a989015c44bc64bd0e6027f3489b4cf127c
Felipe-HVSilva <felype.hen@hotmail.com>
2024-07-30 19:19:42 -0300

    test: ensure GET /api/transactions should return 404 when fetching a transaction from non-existing

--- src/routes/transaction.e2e.test.js
+++ src/routes/transaction.e2e.test.js
@@ -91,9 +91,17 @@ describe('Transaction Routes E2E Tests', () => {
         expect(response.status).toBe(404)
     })
 
-    it('DELETE /api/transactions/:transactionsId should return 404 when updating a non-existing transaction', async () => {
+    it('DELETE /api/transactions/:transactionsId should return 404 when deleting a non-existing transaction', async () => {
         const response = await request(app).delete(
             `/api/transaction/${transaction.id}`,
+        )
+
+        expect(response.status).toBe(404)
+    })
+
+    it('GET /api/transactions?userId should return 404 when fetching a transaction from non-existing user', async () => {
+        const response = await request(app).get(
+            `/api/transactions?userId=${transaction.user_id}`,
         )
 
         expect(response.status).toBe(404)
